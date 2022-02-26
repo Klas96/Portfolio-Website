5bf461863b2176af7bd15c809077bf2babd76e80
Klas Holmgren <klas0holmgren@gmail.com>
2022-02-26 13:50:20 +0100

    Django templates

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html', projects = [1,12,34])),
+    #path('portfolio/',),
+    #path('contact/,'),
 ]
---
+++ main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.urls import URLPattern
+
+# Create your views here.
+
+
+URLPattern = [
+    
+]
