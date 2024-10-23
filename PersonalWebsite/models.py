9268e5ddd0fd1b6eed1d3c9d0a869541a8c58d79
Klas Holmgren <klas0holmgren@gmail.com>
2024-10-23 23:39:41 +0200

    View all fix

---
+++ PersonalWebsite/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+class Certificate(models.Model):
+    title = models.CharField(max_length=255)
+    year = models.CharField(max_length=255)
+    school = models.CharField(max_length=255)
+    href = models.URLField()
+    description = models.TextField()
+
+class Testimonial(models.Model):
+    author = models.CharField(max_length=255)
+    quote = models.TextField()
+    image = models.ImageField(upload_to='testimonials/')
+
+class PersonalDescription(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+class Skill(models.Model):
+    title = models.CharField(max_length=255)
+    percentage = models.IntegerField()
+
+class CodeProject(models.Model):
+    title = models.CharField(max_length=255)
+    year = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='code_projects/')
+    href = models.URLField()
+    skills = models.ManyToManyField(Skill)
+
+class TimelineEntry(models.Model):
+    title = models.CharField(max_length=255)
+    organization = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    time_span = models.CharField(max_length=255)
+    description_points = models.TextField()
